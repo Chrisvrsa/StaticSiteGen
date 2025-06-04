4649c1a860f0f36cf807cc4f1ffdabbab5cf35df
Chrisvrsa <psychosisjr@gmail.com>
2025-06-04 04:07:57 -0400

    Added a way to view props with props_to_html. This shows key value pairs, which represent an html tag(key) such as href and an html value such as https://www.google.com

---
+++ public/src/htmlnode.py
@@ -0,0 +1,27 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        # Child classes will overwrite this method to render HTML
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        items = []
+
+        if self.props is None:
+            return f""
+        for key, value in self.props.items():
+            items.append(f' {key}=\"{value}\"')
+
+        return ''.join(items)
+
+
+    def __repr__(self):
+        return f"{self.tag} {self.value}, {self.children}, {self.props}"
+
+
+
