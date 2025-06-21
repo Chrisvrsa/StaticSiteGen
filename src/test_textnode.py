e7948ae1a1293e782d1ae49166671b67977ca2ad
Chrisvrsa <psychosisjr@gmail.com>
2025-06-20 22:13:06 -0400

    Added unit tests, and a script to run from root directory.

---
+++ src/test_textnode.py
@@ -0,0 +1,22 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_equal(self):
+        node = TextNode("This is a text node", TextType.BOLD, "www.boot.dev.com")
+        node2 = TextNode("This is a text node", TextType.IMAGES, "www.boot.dev.com")
+        self.assertNotEqual(node, node2)
+
+    def test_not_equal_types_and_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.boot.dev.com")
+        self.assertNotEqual(node, node2)
+
+if __name__ == '__main__':
+    # run main in unittest
+    unittest.main()
