e8e7dcc56acd153168cb210aaa0e22d8e3bb3826
Chrisvrsa <psychosisjr@gmail.com>
2025-07-03 04:29:11 -0400

    Add logic to detect headings, code blocks, quotes, unordered and ordered lists in block_to_block_type

---
+++ src/block_type.py
@@ -0,0 +1,64 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(markdown_block_text):
+    valid_headers_list = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
+    
+    for valid_header in valid_headers_list:
+        if markdown_block_text.startswith(valid_header):
+            return BlockType.HEADING
+        
+    if markdown_block_text.startswith("```") and markdown_block_text.endswith("```"):
+        return BlockType.CODE
+    
+    elif markdown_block_text.startswith(">"):
+        split_sections = markdown_block_text.split("\n")
+        split_sections_len = len(split_sections)
+        tally = 0
+
+        for section in split_sections:
+            if section.startswith(">"):
+                tally += 1
+        if tally == split_sections_len:
+            return BlockType.QUOTE
+            
+    elif markdown_block_text.startswith("- "):
+        split_sections = markdown_block_text.split("\n")
+        split_sections_len = len(split_sections)
+        tally = 0
+
+        for section in split_sections:
+            if section.startswith("- "):
+                tally += 1
+        if tally == split_sections_len:
+            return BlockType.UNORDERED_LIST
+        
+        
+    elif markdown_block_text.startswith("1. "):
+        split_sections = markdown_block_text.split("\n")
+        tally = 0
+
+        for index, item in enumerate(split_sections):
+            count = index + 1
+            if item.startswith(f"{count}. "):
+                tally += 1
+        if len(split_sections) == tally:
+            return BlockType.ORDERED_LIST
+            
+
+    # final case
+    else:
+        return BlockType.PARAGRAPH
+
+    
+
+
+
+
