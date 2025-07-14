5ca027d5b0c540bb1671fd1e167e91fbab4c570b
Chrisvrsa <psychosisjr@gmail.com>
2025-07-14 19:04:10 -0400

    Made markdown_to_html_node.py actually return a value instead of cutting off at paragraph. Started working on generate page function at the bottom of main.py to generate a page.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,20 @@ from textnode import TextNode
 from leafnode import LeafNode
 from parent_node import ParentNode
 from textnode import TextType
+from markdown_to_html_node import markdown_to_html_node
 
+"""markdown_to_html_node(markdown) is a function that:
+
+Takes a markdown string as input
+Parses the markdown and converts it into an HTML node structure
+Returns an HTML node object (not a string yet)"""
+
+
+""".to_html() is a method that:
+
+Is called on an HTML node object
+Converts the HTML node structure into an actual HTML string
+Returns the final HTML string that can be written to a file"""
 
 def main():
     src_dir = "static"
@@ -49,6 +62,7 @@ def copy_recursive(src, dst):
             print(f"Copied file: {src} -> {dst}")
             
 
+
 # Extracts the header from the raw markdown
 def extract_title(markdown):
     items = markdown.split("\n")
@@ -58,6 +72,41 @@ def extract_title(markdown):
             item = item[2:].strip()
             return item
     raise Exception("No header found")
+
+
+# should take an abs path
+def generate_page(from_path, template_path, dest_path):
+
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    markdown_content = ""
+    template_content = ""
+
+    # Should end with .md to convert
+    if not from_path.rstrip().endswith(".md"):
+        raise Exception("Markdown content must end with '.md'.")
+    
+    # Get markdown
+    if os.path.exists(from_path):
+        if os.path.isfile(from_path):
+            with open(from_path) as md_file:
+                markdown_content = md_file.read()
+        else:
+            raise Exception("Not a file! Please input a file's absolute path")
+    else:
+        raise Exception("File path does not exist")
+    
+    # Get template
+    if os.path.exists(template_path):
+        if os.path.isfile(template_path):
+            with open(template_path) as temp_file:
+                template_content = temp_file.read()
+
+    # Conversion
+    nodes = markdown_to_html_node(markdown_content)
+    html_conversion = nodes.tohtml()
+    return html_conversion
+
 
 
 
--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -7,71 +7,30 @@ from text_node_to_html_node import text_node_to_html_node
 from text_to_textnodes import text_to_textnodes
 
 def markdown_to_html_node(markdown):
-    # returns a list of markdown blocks
     markdown_blocks = markdown_to_blocks(markdown)
     html_nodes = []
 
     for block in markdown_blocks:
-        # returns an enum type
         block_type = block_to_block_type(block)
 
         match block_type:
             case BlockType.HEADING:
-                # Keep count of each # in the header
-                # valid_headers_list = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
                 count = 0
                 first_elements = block[:6]
 
                 for char in first_elements:
                     if char == "#":
                         count += 1
-                if count == 1:
-                    # each raw value of text needs to be checked for bold, italic or code
-                    # first, clean the value, removing the markdown attributes in valid_headers_list
-                    cleaned_value = block[count+1:].strip()
-
-                    # this returns a list of inline values with proper html formatting
-                    text_nodes = text_to_textnodes(cleaned_value)
-
-                    # this creates actual HTMLNodes (outer, such as h1, h2, etc) from the data in text_nodes
-                    html_children = [text_node_to_html_node(node) for node in text_nodes]
-
-                    # final node with inner nodes appended to the main html_nodes list (on this file)
-                    html_nodes.append(ParentNode(f"h{count}", html_children))
-                    # this process continues below for each #
-
-                elif count == 2:
-                    cleaned_value = block[count+1:].strip()
-                    text_nodes = text_to_textnodes(cleaned_value)
-                    html_children = [text_node_to_html_node(node) for node in text_nodes]
-                    html_nodes.append(ParentNode(f"h{count}", html_children))
-                elif count == 3:
-                    cleaned_value = block[count+1:].strip()
-                    text_nodes = text_to_textnodes(cleaned_value)
-                    html_children = [text_node_to_html_node(node) for node in text_nodes]
-                    html_nodes.append(ParentNode(f"h{count}", html_children))
-                elif count == 4:
-                    cleaned_value = block[count+1:].strip()
-                    text_nodes = text_to_textnodes(cleaned_value)
-                    html_children = [text_node_to_html_node(node) for node in text_nodes]
-                    html_nodes.append(ParentNode(f"h{count}", html_children))
-                elif count == 5:
-                    cleaned_value = block[count+1:].strip()
-                    text_nodes = text_to_textnodes(cleaned_value)
-                    html_children = [text_node_to_html_node(node) for node in text_nodes]
-                    html_nodes.append(ParentNode(f"h{count}", html_children))
-                elif count == 6:
+                if 1 <= count <= 6:
                     cleaned_value = block[count+1:].strip()
                     text_nodes = text_to_textnodes(cleaned_value)
                     html_children = [text_node_to_html_node(node) for node in text_nodes]
                     html_nodes.append(ParentNode(f"h{count}", html_children))
             
             case BlockType.CODE:
-                # remove backticks from markdown, and pass it in as a value to the child html node.
                 cleaned_code_block = block.replace("```", "")
                 new_node = ParentNode("pre", [
                     HTMLNode("code", cleaned_code_block)
-                    
                 ])
                 html_nodes.append(new_node)
             
@@ -86,12 +45,10 @@ def markdown_to_html_node(markdown):
                         cleaned = section.lstrip()
                     list_of_cleaned_sections.append(cleaned)
 
-                # we have raw values already
-                joined_quote ="\n".join(list_of_cleaned_sections).strip()
+                joined_quote = "\n".join(list_of_cleaned_sections).strip()
                 text_nodes = text_to_textnodes(joined_quote)
                 html_children = [text_node_to_html_node(node) for node in text_nodes]
                 html_nodes.append(ParentNode("blockquote", html_children))
-
 
             case BlockType.UNORDERED_LIST:
                 valid_unordered_list = ["* ", "- ", "+ "]
@@ -99,26 +56,23 @@ def markdown_to_html_node(markdown):
 
                 for section in block.split("\n"):
                     section = section.lstrip()
-                    if not section == "":
+                    if section != "":
                         for marker in valid_unordered_list:
                             if section.startswith(marker):
-                                # remove markdown syntax, raw value
                                 content = section[len(marker):].lstrip()
                                 text_nodes = text_to_textnodes(content)
                                 html_children = [text_node_to_html_node(node) for node in text_nodes]
                                 li_nodes.append(ParentNode("li", html_children))
-                                break # only match the first matched marker 
-                    # Make the ul node, passing the list of li_nodes as children
-                
+                                break
                 html_nodes.append(ParentNode("ul", li_nodes))
 
             case BlockType.ORDERED_LIST:
                 li_values = block.split("\n")
                 li_nodes = []
-                
+
                 for value in li_values:
-                    if not value == "": 
-                    # strip the markdown values and generate raw values (text)
+                    value = value.lstrip()
+                    if value != "":
                         first_space = value.find(' ')
                         if first_space != -1 and value[:first_space].endswith('.'):
                             extract_value = value[first_space+1:]
@@ -136,15 +90,5 @@ def markdown_to_html_node(markdown):
                 text_nodes = text_to_textnodes(block)
                 html_children = [text_node_to_html_node(node) for node in text_nodes]
                 html_nodes.append(ParentNode("p", html_children))
-                
 
-
-
-
-            
-                
-
-
-
-                
-
+    return ParentNode("div", html_nodes)
